2f2e647e0ef9707d5217d9e8edb8b86c4405b5d5
Сергей Корзуев <ksa.po4ta@gmail.com>
2022-05-03 20:28:58 +0300

    ver.2.0

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,20 +1,22 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Group
 
-LIMITATION_OUTPUT = 10
+LIMITATION = 10
+
 
 def index(request):
     template = 'posts/index.html'
-    posts = Post.objects.order_by('-pub_date')[:LIMITATION_OUTPUT]
+    posts = Post.objects.order_by('-pub_date')[:LIMITATION]
     context = {
         'posts': posts,
     }
     return render(request, template, context)
 
+
 def group_posts(request, slug):
     template = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:LIMITATION_OUTPUT]
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:LIMITATION]
     context = {
         'group': group,
         'posts': posts,
