44989f98cf462e48e09e7447fd2fd73b474a5603
Сергей Корзуев <ksa.po4ta@gmail.com>
2022-05-04 20:13:33 +0300

    ver.2.3

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,15 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+
 from .models import Post, Group
 
 
 class PostAdmin(admin.ModelAdmin):
-    # Перечисляем поля, которые должны отображаться в админке
     list_display = ('pk', 'text', 'pub_date', 'author', 'group')
-    # Добавляем интерфейс для поиска по тексту постов
     search_fields = ('text',)
-    # Добавляем возможность фильтрации по дате
     list_filter = ('pub_date',)
     list_editable = ('group',)
     empty_value_display = '-пусто-'
